b935c6c9339962edf8db47850108975c5cb6d9c5
lisette-18 <horne146@umn.edu>
2024-02-25 18:35:05 -0600

    First 4 Stages of Data Cleaning

--- R/week6.R
+++ R/week6.R
@@ -10,3 +10,11 @@ citations <- stri_read_lines("../data/citations.txt", encoding = 'Windows-1252')
 citations_txt <- citations[!stri_isempty(citations)]
 length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
+
+#Data Cleaning
+view(sample_n(citations_tbl, 20))
+
+citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_replace_all(cite, pattern = "[\'\"]", replacement = ""))%>%
+  mutate(year = str_extract(cite, pattern = "\\d{4}")) %>%
+  mutate(page_start = str_extract(cite, pattern = "\\d+(?=-\\d+)"))
