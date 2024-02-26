51929823eafca0a2b6915239d5138e5dee7104f1
lisette-18 <horne146@umn.edu>
2024-02-25 18:04:58 -0600

    Data Import Complete

--- R/week6.R
+++ R/week6.R
@@ -2,3 +2,11 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
 library(tidyverse)
+
+
+
+#Data Import
+citations <- stri_read_lines("../data/citations.txt", encoding = 'Windows-1252')
+citations_txt <- citations[!stri_isempty(citations)]
+length(citations) - length(citations_txt)
+mean(str_length(citations_txt))
